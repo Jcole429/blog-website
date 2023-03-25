785c228324d2cc3c29413d5ebf74d144d280ef7c
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-25 14:19:48 -0400

    Render starting home content

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { content: homeStartingContent });
 });
 
 app.listen(PORT, function () {
